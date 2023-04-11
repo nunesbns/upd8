cc6693442aef50488300589c6b9176adf6e24547
Bruno Nunes de Santana <65-bruno@users.noreply.git.edinheiro.org>
2023-04-10 21:40:21 -0300

    Add acronym column in states table

--- database/migrations/2023_04_10_235445_create_states_table.php
+++ database/migrations/2023_04_10_235445_create_states_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('states', function (Blueprint $table) {
             $table->id();
             $table->string('name', 15)->nullable(false);
+            $table->string('acronym', 2)->nullable(false);
         });
     }
 
