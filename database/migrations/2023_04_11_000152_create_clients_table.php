6174dd3e19cedf790337df929719740f2218bdc3
Bruno Nunes de Santana <65-bruno@users.noreply.git.edinheiro.org>
2023-04-10 21:08:29 -0300

    Add migrations

---
+++ database/migrations/2023_04_11_000152_create_clients_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('clients', function (Blueprint $table) {
+            $table->id();
+            $table->string('identity', 11)->nullable(false)->unique()->comment('CPF do cliente');
+            $table->string('name', 50)->nullable(false);
+            $table->timestamp('birthdate')->nullable(false);
+            $table->enum('gender', ['male', 'female'])->nullable(false);
+            $table->unsignedBigInteger('city_id')->nullable(false);
+            $table->timestamps();
+            $table->foreign('city_id')->references('id')->on('cities');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('clients');
+    }
+};
